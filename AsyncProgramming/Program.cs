c8c1a45cc7fc47a7998367cb9d7aa3bcd1b5c086
Chinenye <c.okeke@genesystechhub.com>
2022-12-12 11:43:17 +0100

    Add project files.

---
+++ AsyncProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AsyncProgramming
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's learn Async Programming!");
+
+            Console.ReadKey();
+        }
+    }
+}
